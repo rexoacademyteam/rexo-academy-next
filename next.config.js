2abe830decb1b1121db87aee140593d23c631b64
GeekyChunk <ermia1303@gmail.com>
2023-07-04 14:53:50 +0330

    captcha

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    BASEAPI: process.env.BASEAPI,
+    CAPTCHA_SITEKEY: process.env.CAPTCHA_SITEKEY
+  },
+  env: {
+    BASEAPI: process.env.BASEAPI,
+    CAPTCHA_SITEKEY: process.env.BASEAPI,
+
+  }
 }
 
 module.exports = nextConfig
